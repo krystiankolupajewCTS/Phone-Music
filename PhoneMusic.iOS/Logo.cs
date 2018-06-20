18e1ca9ef8332e1b242dc984dfbb94abd4f7b494
krystiankolupajewCTS <32029@corbytechnicalschool.org>
2018-06-20 13:31:00 +0100

    Updated Loading Screen

---
+++ PhoneMusic.iOS/Logo.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace PhoneMusic.iOS
+{
+    public partial class Logo : UIViewController
+    {
+        public Logo (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
